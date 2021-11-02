9d13d210dd7af9fb5c87d0cdaefcfc74bd477246
KyleTownsend74 <townsend.kyle.kt@gmail.com>
2021-11-01 18:52:24 -0700

    Add feature to display article data from API

--- script.js
+++ script.js
@@ -7,11 +7,35 @@ async function getArticles() {
 // Display articles from API request
 async function displayArticles() {
     try {
+        // Get articles to display
         const articles = await getArticles();
+
+        // Set up variables to display content
+        const mainContent = document.querySelector("main");
+        let title;
+        let imageUrl;
+        let summary;
 
         // Parse through article data
         for(let article of articles) {
-            console.log(article.title);
+            // Assign proper values to variables
+            title = article.title;
+            imageUrl = article.imageUrl;
+            summary = article.summary;
+
+            // Create and insert the HTML
+            mainContent.insertAdjacentHTML("afterbegin", 
+                `<div class="item box">
+                <img src="${imageUrl}" alt="article image">
+                <div class="item-text">
+                    <h2>
+                        ${title}
+                    </h2>
+                    <p>
+                        ${summary}
+                    </p>
+                </div>
+            </div>`);
         }
     } catch(e) {
         console.log("Error getting article data:", e);
